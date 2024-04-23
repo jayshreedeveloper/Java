362f18ee182fdb0ca1539a5989d2db610da5f1f4
jayshreedeveloper <167888075+jayshreedeveloper@users.noreply.github.com>
2024-04-23 14:24:47 +0000

    9

--- .devcontainer/Basic.java
+++ .devcontainer/Basic.java
@@ -1,8 +1,24 @@
+class hello{
+
+
+   void Greet()
+   {
+
+    System.out.println("Jai Shree ram");
+   }
+}
+
+
 public class Basic {
 
 	public static void main(String[] args) {
         
   System.out.println("Jai Shree krishna ");
+
+
+  hello obj=new hello();
+
+  obj.Greet();
 
 
     }}
